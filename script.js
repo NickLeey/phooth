7ed8625341dc72e290bc44aa2dddeca29a0767eb
NickLeey <134098581+NickLeey@users.noreply.github.com>
2024-02-05 09:19:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function addReaction(element) {
     let currentCount = parseInt(element.textContent.trim().split(' ')[1]);
     element.innerHTML = `❤️ ${++currentCount}`;
 }
+function signInWithGoogle() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
